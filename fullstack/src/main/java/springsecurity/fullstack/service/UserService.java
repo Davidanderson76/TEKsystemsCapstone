5d035d606eb3b292bf5d4c94819c5ff7e47f9f42
david <david.anderson1993@gmail.com>
2022-06-15 15:36:29 -0500

    running application

--- fullstack/src/main/java/springsecurity/fullstack/service/UserService.java
+++ fullstack/src/main/java/springsecurity/fullstack/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import springsecurity.fullstack.UserRegistrationDto;
 import springsecurity.fullstack.entity.User;
 
+
 public interface UserService extends UserDetailsService {
     User findByEmail(String email);
     User save(UserRegistrationDto registration);
